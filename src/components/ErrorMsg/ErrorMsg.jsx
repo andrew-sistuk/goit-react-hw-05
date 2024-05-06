4b82385d50dfe118f0493273b59d41aaa8e34bf3
Wolf <andresistuk@gmail.com>
2024-05-06 14:55:59 +0300

    fix reload

--- src/components/ErrorMsg/ErrorMsg.jsx
+++ src/components/ErrorMsg/ErrorMsg.jsx
@@ -1,19 +1,18 @@
 import { Link, useLocation } from 'react-router-dom';
 import clsx from 'clsx';
-// import { reload } from '../../utils/reload';
 
 import css from './ErrorMsg.module.css';
 
 const ErrorMsg = () => {
-  const location = useLocation();
+  const {pathname, state} = useLocation();
   return (
     <div className={clsx(css.notfound,'container')}>
       <img className={css['main-img']} src="/src/assets/error.png" alt="404 not found" />
       <div className={css.links}>
-        <Link to={location.state ? location.state : '/'} className={clsx(css.button, css.red)}>
+        <Link to={state ? state : '/'} className={clsx(css.button, css.red)}>
           Go Back
         </Link>
-        <Link to={location} className={clsx(css.button, css.blue)}>
+        <Link to={pathname} className={clsx(css.button, css.blue)} onClick={() => window.location.reload()}>
           Reload
         </Link>
       </div>
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -4,15 +4,15 @@ import clsx from 'clsx';
 import css from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
-  const location = useLocation();
+  const { state } = useLocation();
   return (
-    <section className={clsx(css.notfound,'container')}>
+    <section className={clsx(css.notfound, 'container')}>
       <img className={css['main-img']} src="/src/assets/404.png" alt="404 not found" />
       <div className={css.links}>
         <Link to="/" className={clsx(css.button, css.red)}>
           Go Home page
         </Link>
-        <Link to={location.state ? location.state : '/movies'} className={clsx(css.button, css.blue)}>
+        <Link to={state ? state : '/movies'} className={clsx(css.button, css.blue)}>
           Go Back
         </Link>
       </div>
